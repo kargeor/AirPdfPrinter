dbad7220590e49e3e5f1aad7d3177b6b9b018efd
GEORGIOS KARNAS <kargeor@gmail.com>
2024-10-13 10:34:07 -0700

    share

--- config.sh
+++ config.sh
@@ -10,4 +10,17 @@ lpadmin -p $PTR -v cups-pdf:/ -E -P /usr/share/ppd/cups-pdf/CUPS-PDF_opt.ppd
 echo "Setting the default printer to $PTR."
 lpadmin -d $PTR
 
+
+
+### https://www.cups.org/doc/sharing.html
+
+cupsctl --share-printers --remote-any
+
+lpadmin -p PDF -o printer-is-shared=true
+
+
+###
+
+
+
 service cups stop
